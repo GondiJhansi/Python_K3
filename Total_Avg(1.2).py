e19d7fb46357f90c1feed6f1c1dc1b408dc513f5
GondiJhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-11-01 00:14:06 -0700

    Add files via upload

---
+++ Total_Avg(1.2).py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter 1st number="))
+num2 = int(input("Enter 2nd number="))
+num3 = int(input("Enter 3rd number="))
+total = num1+num2+num3
+avg = total/3
+print("Total=",total)
+print("Average",avg)
