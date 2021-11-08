2d3545e6a46dc3163d95c15a0b2ddc3c375348c9
Jhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-11-07 22:10:20 -0800

    Add files via upload

---
+++ Armstrong(2.3).py
@@ -0,0 +1,14 @@
+num = int(input("Enter a number ="))
+sum=0
+temp = num
+while (temp>0):
+    rem = temp % 10
+    sum = sum+(rem*rem*rem)
+    temp = temp // 10
+
+if num==sum:
+    print("Armstrong number")
+else:
+    print("Not Armstrong number")
+
+
