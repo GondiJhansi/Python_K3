c398544f542f48b75badc38b82771a1cff8c9fe2
Jhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-11-14 22:19:49 -0800

    Add files via upload

--- String_Length(4.2).py
+++ String_Length(4.2).py
@@ -1,5 +1,5 @@
-s1=input("Enter 1st string")
-s2=input("Enter 2nd string")
+s1=input("Enter 1st string ")
+s2=input("Enter 2nd string ")
 l1=len(s1)
 l2=len(s2)
 if l1!=l2:
@@ -7,5 +7,5 @@ if l1!=l2:
 else:
     s=" "
     for i in range(l1):
-        s+=s2[i]+s1[i]
-        print(s)
+        s=s1[i]+s2[i]
+        print(s,end="")
