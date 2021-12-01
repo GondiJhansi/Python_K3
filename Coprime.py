1ec9e930fb90d4507767cb40b13dd7f4f76da7ae
Jhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-12-01 17:40:44 +0530

    Add files via upload

---
+++ Coprime.py
@@ -0,0 +1,12 @@
+def gcd(a,b):
+    while True:
+        if a>b:
+            a,b=b,a
+        b=b%a
+        if b==0:
+            return a
+a,b=map(int,input().split())
+if (a%2 or b%2) and gcd(a,b)==1:
+    print("Coprimes")
+else:
+    print("Not a coprime")
