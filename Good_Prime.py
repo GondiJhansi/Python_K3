ca4fdfe4044ae38edba7f47877ef6d84518c7be1
Jhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-12-01 17:09:28 +0530

    Add files via upload

---
+++ Good_Prime.py
@@ -0,0 +1,31 @@
+from math import *
+def isprime(num):
+    if num==1:
+        return False
+    sq=int(sqrt(num))
+    for i in range(2,sq+1):
+        if num%i==0:
+            return False
+    return True
+num=int(input())
+
+if isprime(num):
+    print("Prime")
+    np=num+2
+    while True:
+        if isprime(np):
+            break
+        else:
+            np+=2
+    pp=num-2
+    while True:
+        if isprime(pp):
+            break
+        else:
+            pp-=2
+    if np*pp<=num*num:
+            print("good prime")
+    else:
+            print("baad prime")
+else:
+    print("Not Prime")
