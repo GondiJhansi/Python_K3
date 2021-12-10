18a01ec6bac42f91661e4ea2c30828ef1a2c9e69
Jhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-12-10 17:47:02 +0530

    Add files via upload

---
+++ dic_Common_Numbers.py
@@ -0,0 +1,16 @@
+n1=int(input())
+data1=list(map(int,input().split()))
+n2=int(input())
+data2=list(map(int,input().split()))
+dic={}
+for i in data1:
+    dic[i]=1
+for i in data2:
+    if i not in dic:
+        dic[i]=1
+    else:
+        dic[i]=0
+for k,v in dic.items():
+    if v==0:
+        print(k,end=" ")
+
