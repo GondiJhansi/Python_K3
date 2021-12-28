8a85170af5667ca816a4c4ab2fb34ac45ff21671
Jhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-12-28 17:23:17 +0530

    Add files via upload

---
+++ Stri_Cap_Count2.py
@@ -0,0 +1,12 @@
+s=input()
+d=list(s)
+n=len(d)
+i=1
+d[0]=d[0].upper()
+while i<n:
+    if ord(d[i])>=65 and ord(d[i])<=90:
+           d.insert(i,' ')
+           i+=2
+    else:
+        i+=1
+print(''.join(d))
